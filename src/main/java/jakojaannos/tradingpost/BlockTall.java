f515aad0863f8277160f106c5d4e34ccfbed5ae0
Lassi <lassi.kuisma1@gmail.com>
2017-11-26 01:24:40 +0200

    asd

--- src/main/java/jakojaannos/tradingpost/BlockTall.java
+++ src/main/java/jakojaannos/tradingpost/BlockTall.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
+import net.minecraft.block.material.EnumPushReaction;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyBool;
 import net.minecraft.block.state.BlockStateContainer;
@@ -11,6 +12,7 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
@@ -24,7 +26,6 @@ public class BlockTall extends Block {
 		setSoundType(SoundType.WOOD);
 		setUnlocalizedName("TallBlock");
 		setDefaultState(this.blockState.getBaseState().withProperty(isTop, false));
-		// setCreativeTab(TRDCreativeTabs.DEVNULL);
 	}
 
 	public BlockStateContainer createBlockState() {
@@ -38,68 +39,54 @@ public class BlockTall extends Block {
 	public void onBlockHarvested(World worldIn, BlockPos pos, IBlockState state, EntityPlayer player) {
 		BlockPos blockDown = pos.down();
 		BlockPos blockUp = pos.up();
-		
-		
 
-		// this is the top block -> check if there is a bottom block under it and destroy it
+		// this is the top block -> check if there is a bottom block under it
+		// and destroy it
 		if (state.getValue(isTop) && worldIn.getBlockState(blockDown).getBlock() == this) {
 			worldIn.setBlockToAir(blockDown);
 		} else {
-			if(worldIn.getBlockState(blockUp).getBlock()==this){
+			if (worldIn.getBlockState(blockUp).getBlock() == this) {
 				worldIn.setBlockToAir(blockUp);
 			}
 		}
-		
-		
-		
 
-//		if (player.capabilities.isCreativeMode && state.getValue(isTop)
-//				&& worldIn.getBlockState(blockDown).getBlock() == this) {
-//			worldIn.setBlockToAir(blockDown);
-//		}
-//
-//		if (!state.getValue(isTop) && worldIn.getBlockState(blockUp).getBlock() == this) {
-//			if (player.capabilities.isCreativeMode) {
-//				worldIn.setBlockToAir(pos);
-//			}
-//
-//			worldIn.setBlockToAir(blockUp);
-//		}
+	}
+
+	public EnumPushReaction getMobilityFlag(IBlockState state) {
+		return EnumPushReaction.DESTROY;
+	}
+
+	public void neighborChanged(IBlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos fromPos) {
+		BlockPos blockDown = pos.down();
+		BlockPos blockUp = pos.up();
+
+		boolean destroy = false;
+
+		if (state.getValue(isTop) && worldIn.getBlockState(blockDown).getBlock() != this) {
+			// upper half, the block below me is not BlockTall
+			worldIn.setBlockToAir(pos);
+			destroy = true;
+		} else {
+			// lower half
+			if (worldIn.getBlockState(blockUp).getBlock() != this || //
+			// the block above me is not BlockTall
+					!worldIn.getBlockState(blockDown).isSideSolid(worldIn, blockDown, EnumFacing.UP)) {
+				// the block below me is not solid
+				worldIn.setBlockToAir(pos);
+				destroy = true;
+			}
+		}
+
+		if (destroy && !worldIn.isRemote) {
+			this.dropBlockAsItem(worldIn, pos, state, 0);
+		}
+
 	}
 
 	public Item getItemDropped(IBlockState state, Random rand, int fortune) {
 
-		return state.getValue(isTop) ? Items.AIR : TRDBlocks.ITEM_TALL;
+		return TRDBlocks.ITEM_TALL;
 	}
-	
-//	  /**
-//     * Called when the block is right clicked by a player.
-//     */
-//    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
-//    {
-//        if (this.blockMaterial == Material.IRON)
-//        {
-//            return false;
-//        }
-//        else
-//        {
-//            BlockPos blockpos = state.getValue(HALF) == BlockDoor.EnumDoorHalf.LOWER ? pos : pos.down();
-//            IBlockState iblockstate = pos.equals(blockpos) ? state : worldIn.getBlockState(blockpos);
-//
-//            if (iblockstate.getBlock() != this)
-//            {
-//                return false;
-//            }
-//            else
-//            {
-//                state = iblockstate.cycleProperty(OPEN);
-//                worldIn.setBlockState(blockpos, state, 10);
-//                worldIn.markBlockRangeForRenderUpdate(blockpos, pos);
-//                worldIn.playEvent(playerIn, ((Boolean)state.getValue(OPEN)).booleanValue() ? this.getOpenSound() : this.getCloseSound(), pos, 0);
-//                return true;
-//            }
-//        }
-//    }
 
 	public int getMetaFromState(IBlockState state) {
 		if (state.getValue(isTop))
