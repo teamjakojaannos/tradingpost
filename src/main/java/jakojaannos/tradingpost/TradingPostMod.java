23964ee4a19a62fdab52c38eb703129c562634ff
Kailari <ilari97@hotmail.com>
2017-11-26 00:06:26 +0200

    Updated proxy paths

--- src/main/java/jakojaannos/tradingpost/TradingPostMod.java
+++ src/main/java/jakojaannos/tradingpost/TradingPostMod.java
@@ -21,7 +21,7 @@ public class TradingPostMod {
     public static final String VERSION = "0.0.0";
     
 	
-	@SidedProxy(clientSide = "ClientProxy", serverSide = "CommonProxy")
+	@SidedProxy(clientSide = "jakojaannos.tradingpost.ClientProxy", serverSide = "jakojaannos.tradingpost.CommonProxy")
 	public static CommonProxy proxy;
 	
 	@Instance(MODID)
